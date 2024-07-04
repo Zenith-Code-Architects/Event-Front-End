a9714989d4c5579de611c605217f1df7f67e39f3
jayjehu <jehularyea@gmail.com>
2024-07-04 14:06:00 +0000

    edited admin form

--- src/pages/event-creation/components/AdminForm.jsx
+++ src/pages/event-creation/components/AdminForm.jsx
@@ -4,13 +4,13 @@ const AdminForm = () => {
     return (
 
 
-        <form class="w-full max-w-lg m-auto">
+        <form class="w-full max-w-lg m-auto mt-8">
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-name">
-                        Event Name
+                        Event Name<span className="required">*</span>:
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-name" type="text" placeholder="Event Caption" /> {/* <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-name" type="text" placeholder="Event Caption" required /> {/* <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
                         */}
                 </div>
             </div>
@@ -19,45 +19,48 @@ const AdminForm = () => {
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-date">
                         Event Date
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-date" type="date" placeholder="Doe" />
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-date" type="date" placeholder="Date" />
                 </div>
 
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-time">
                         Event Time
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="time" placeholder="Time" />
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="event-time" type="time" placeholder="Time" />
                     <p class="text-red-500 text-xs italic">Please fill out this field.</p>
                 </div>
 
             </div>
 
-            <div class="flex flex-wrap -mx-3 mb-2">
-                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-place">
-                        location
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-location">
+                        Location
                     </label>
-                    <input class="appearance-none block w-[300px] bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-place" type="text" placeholder="City & Digital Address" />
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-location" type="text" placeholder="City & Digital Address" />
                 </div>
 
-
-
-                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-price">
                         Price
                     </label>
-                    <input class="appearance-none block  w-[300px] bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-price" type="text" placeholder="Price" />
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="event-price" type="text" placeholder="Price" />
+
                 </div>
 
-                
             </div>
 
-            <div class="w-full md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-date">
-                        Event Date
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="event-flier">
+                        Flier
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-flier" type="file" placeholder="Doe" />
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="event-flier" type="file" placeholder="Event Flier" />
                 </div>
+                <div className='mx-auto mt-8'>
+                    <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white bg-[#793203] shadow-black hover:border-transparent hover:text-white-600 hover:bg-[#f79456] mt-4 lg:mt-0">Post Updates</a>
+                </div>
+            </div>
 
         </form>
     )
--- src/pages/event-creation/components/Header.jsx
+++ src/pages/event-creation/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div>
         <nav class="flex items-center justify-between flex-wrap bg-[#793203] p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    {/* <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
     <span class="font-semibold text-xl tracking-tight">ZCA EVENTS</span>
   </div>
   <div class="block lg:hidden">
@@ -15,13 +15,13 @@ const Header = () => {
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-amber-600 mr-4">
+      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-amber-600 mr-4">
         Home
       </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-amber-600 mr-4">
+      <a href="admin-dashboard" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-amber-600 mr-4">
         Dashboard
       </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-amber-600">
+      <a href="all-events" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-amber-600">
         All Events
       </a>
     </div>
