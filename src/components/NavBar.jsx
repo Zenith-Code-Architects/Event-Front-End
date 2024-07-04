bc0f45f6f7d7fc3ea68ef5d68a76c1f0b22b5e5b
rilian <rilian0031@gmail.com>
2024-07-04 13:26:39 +0000

    show the updated navbar and other links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Nav from '../constants'
+import { Link } from 'react-router-dom'
+import Logo from "../assets/images/logo.jpg"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='flex justify-between fixed w-full px-20 py-10 bg-amber-900'>
+      <img src={Logo} alt="logo" className='h-12 w-auto'/>
+<ul className="nav-links">
+                {Nav.NAVLINKS.map(link => (
+                    <li key={link.name}>
+                        <a href={link.path} className="nav-link">{link.name}</a>
+                    </li>
+                ))}
+            </ul>
+      
+   
+    </div>
   )
 }
 
---
+++ src/constants/index.jsx
@@ -0,0 +1,30 @@
+
+const Nav = {
+    NAVLINKS:[
+        {
+            name:"Home",
+            path:"/"
+        },
+        {
+            name:"About",
+            path:"/about-us"
+        },
+        {
+            name:"Events",
+            path:"/events"
+        },
+        {
+            name:"Contact Us",
+            path:"/contact-us"
+        },
+        {
+            name:"Create Event +",
+            path:"event-creation"
+        },
+
+
+    ]
+
+}
+
+export default Nav
